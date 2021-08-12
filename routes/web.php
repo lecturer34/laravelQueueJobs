1c8e64cd8e177b549d3b834d81be765ccea75a16
Aminu Abdulsalami <lecturer34@gmail.com>
2021-08-12 21:41:11 +0100

    Finalizing changes

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 use Carbon\Carbon;
 
-Route::get('/upload', [CustomerController::class, 'index'])->name('upload');
-Route::post('/upload', [CustomerController::class, 'store']);
+Route::get('/', [CustomerController::class, 'index'])->name('upload');
+Route::post('/', [CustomerController::class, 'store']);
